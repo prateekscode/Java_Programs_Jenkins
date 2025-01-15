113f92d3c6669e7142cacc31e50be72c99c1fa3f
Prateek Gupta <prateekgupta2002@gmail.com>
2025-01-15 21:31:25 +0530

    Test Program

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main{
+public static void main(String[] args){
+System.out.prinln("This is java program");
+System.out.prinln("Output from java program");
+System.out.prinln(new Date());
+}
+}
