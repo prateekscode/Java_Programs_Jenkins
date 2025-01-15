79f8017b36f68d95de283846458dc1abe57c343c
Prateek Gupta <prateekgupta2002@gmail.com>
2025-01-15 21:51:51 +0530

    Adde stream logic

--- Main.java
+++ Main.java
@@ -4,5 +4,14 @@ public static void main(String[] args){
 System.out.println("This is java program");
 System.out.println("Output from java program");
 System.out.println(new Date());
+
+List<Integer> al = new ArrayList<Integer>();
+al.add(15);
+al.add(44);
+al.add(32);
+al.add(51);
+
+al.stream().filter(x -> x % 2 == 0).forEach(x -> System.out.println(x));
+al.stream().map(x->x*2).forEach(n->System.out.println("-------"+n));
 }
 }
